f8e7f0da88c67c2e7e12aef960fc412cd732a725
MH4GF <h.miyagi.cnw@gmail.com>
2024-06-16 19:12:08 +0900

    maintenance: setup project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import reactDom from "react-dom/client";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = document.getElementById("root");
+if (!root) {
+  throw new Error("Root element not found");
+}
+
+reactDom.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
