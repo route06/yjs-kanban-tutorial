dcd29a9250f7070dc04e574c508040ba63390504
MH4GF <h.miyagi.cnw@gmail.com>
2024-06-23 22:19:13 +0900

    feat:implement task ordering logic

--- src/taskStore.ts
+++ src/taskStore.ts
@@ -6,15 +6,26 @@ interface TaskStore {
   [taskId: string]: Task;
 }
 
+const computeOrder = (prevId?: string, nextId?: string): number => {
+  const prevOrder = taskStore[prevId ?? ""]?.order ?? 0;
+  const nextOrder = nextId ? taskStore[nextId].order : 1;
+
+  return (prevOrder + nextOrder) / 2;
+};
+
 export const filteredTasks = (status: TaskStatus, taskStore: TaskStore): Task[] =>
-  Object.values(taskStore).filter((task) => task.status === status);
+  Object.values(taskStore)
+    .filter((task) => task.status === status)
+    .sort((a, b) => a.order - b.order);
 
 export const taskStore = proxy<TaskStore>({});
 
 export const useTasks = () => useSnapshot(taskStore);
 
 export const addTask = (status: TaskStatus) => {
-  const order = 0; // dummy
+  const tasks = filteredTasks(status, taskStore);
+  const lastTask = tasks[tasks.length - 1];
+  const order = computeOrder(lastTask?.id);
   const id = nanoid();
   taskStore[id] = {
     id,
