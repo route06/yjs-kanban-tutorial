682e04226069db376aa7e0d1c9d8ba963a98e5d9
MH4GF <h.miyagi.cnw@gmail.com>
2024-06-23 22:21:43 +0900

    feat: add moveTask

--- src/taskStore.ts
+++ src/taskStore.ts
@@ -41,3 +41,12 @@ export const updateTask = (id: string, value: string) => {
     task.value = value;
   }
 };
+
+export const moveTask = (id: string, status: TaskStatus, prevId?: string, nextId?: string) => {
+  const order = computeOrder(prevId, nextId);
+  const task = taskStore[id];
+  if (task) {
+    task.status = status;
+    task.order = order;
+  }
+};
