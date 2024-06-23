5176efc822a15a3f82cb4006808cffa028457090
MH4GF <h.miyagi.cnw@gmail.com>
2024-06-23 18:36:17 +0900

    feat: Change to use snapshot from taskStore

--- src/TaskColumn.tsx
+++ src/TaskColumn.tsx
@@ -2,19 +2,16 @@ import type { FC } from "react";
 import { TaskAddButton } from "./TaskAddButton";
 import styles from "./TaskColumn.module.css";
 import { TaskItem } from "./TaskItem";
-import type { Task, TaskStatus } from "./types";
+import { filteredTasks, useTasks } from "./taskStore";
+import type { TaskStatus } from "./types";
 
 interface Props {
   status: TaskStatus;
 }
 
 export const TaskColumn: FC<Props> = ({ status }) => {
-  // TODO
-  const tasks: Task[] = [
-    { id: "1", status, value: "Task 1", order: 1 },
-    { id: "2", status, value: "Task 2", order: 2 },
-    { id: "3", status, value: "Task 3", order: 3 },
-  ];
+  const snapshot = useTasks();
+  const tasks = filteredTasks(status, snapshot);
 
   return (
     <div className={styles.wrapper}>
